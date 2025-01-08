b23f0d6bbe156762b9c1c9320ac23906674436b0
sinseongmin <23_10514@daejin.sen.hs.kr>
2025-01-08 11:33:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ st.write('안녕하시오. 나는 □□이다')
 st.write('나의 이메일 주소는 23_10514@daejin.sen.hs.kr 이다')
 
 st.markdown(
-    """
     <style>
     .highlight-text {
         color: red;
@@ -15,7 +14,7 @@ st.markdown(
         font-size: 50px;
     }
     </style>
-    """,
+,
     unsafe_allow_html=True,
 )
 
