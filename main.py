32ee87346703e6097029abe5815129b90e2cec8c
sinseongmin <23_10514@daejin.sen.hs.kr>
2025-01-08 11:53:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,7 @@ image_url = "https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjIy/MDAxNjQzMTAyO
 st.image(image_url, use_column_width=True)
 
 st.button("초기화", type="primary")
-if st.button("ㄴmport streamlit as st
-import random
-import webbrowser
 
-st.title("나의 첫번째 앱")
-st.text('／n／n')
-st.write('안녕하시오. 나는 □□이다')
-st.write('나의 이메일 주소는 23_10514@daejin.sen.hs.kr 이다')
-
-image_url = "https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjIy/MDAxNjQzMTAyOTg1MTc0.iCRKrVZvLQgyEfwKYVyGDQAM5R6Rm8Kbh6KOr9Rc2ykg.8-XafjqGUwLSQIa-G_8L6wonoOGwJBmKqAA0yLIOGWMg.JPEG.minziminzi128/IMG_7373.JPG?type=w800"
-st.image(image_url, use_column_width=True)
-
-st.button("초기화", type="primary")
 if st.button("무작위 나무위키 열기"):
     random_url = "https://namu.wiki/random"
     webbrowser.open(random_url)
