edbb56642d7e818e51f6fa73c36e0222776a3cfc
sinseongmin <23_10514@daejin.sen.hs.kr>
2025-01-08 11:33:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,19 @@ st.text('／n／n')
 st.write('안녕하시오. 나는 □□이다')
 st.write('나의 이메일 주소는 23_10514@daejin.sen.hs.kr 이다')
 
+st.markdown(
+    """
+    <style>
+    .highlight-text {
+        color: red;
+        font-weight: bold;
+        font-size: 50px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 st.button("초기화", type="primary")
 if st.button("난수 생성"):
     st.write(random.randint(1,1000))
