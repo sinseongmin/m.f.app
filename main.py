8650eaeabcafc03675ee375d4b6c716cee297e31
sinseongmin <23_10514@daejin.sen.hs.kr>
2025-01-08 11:22:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ st.write('나의 이메일 주소는 23_10514@daejin.sen.hs.kr 이다')
 
 st.button("초기화", type="primary")
 if st.button("난수 생성"):
-    st.write(random.randint(1,1000)
+    st.write(random.randint(1,1000))
 else:
     st.write("잘가")
