29e67bfb0a74432841d26b75abb746318e77fb36
sinseongmin <23_10514@daejin.sen.hs.kr>
2025-01-08 11:42:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.text('／n／n')
 st.write('안녕하시오. 나는 □□이다')
 st.write('나의 이메일 주소는 23_10514@daejin.sen.hs.kr 이다')
 
-image_url = "https://i.namu.wiki/i/DxxnonYmddt8njMYPO0Mogb0eGS-4rxun2-6godbXnMT78C8BXNvYxQZHiVCQ_s64yhi9mCDesCzdi0Ods-F5tEr_Z3ccQUw7rzBJAUYZNlckDwafJZBW_-WOSbAOm9hHGWOuGsUxUNqbaOQ6XXU2w.webp"
+image_url = "https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fmy-cat-wakes-me-up-every-morning-but-today-i-woke-him-up-v0-vd3umbwyfmbe1.jpg%3Fwidth%3D640%26crop%3Dsmart%26auto%3Dwebp%26s%3D7a75550c5eaba3ff6c056912f72abd3ec7252b18"
 st.image(image_url, use_column_width=True)
 
 st.button("초기화", type="primary")
